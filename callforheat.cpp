413e9f5e78400960716c9f2b5d7bda3f0082754e
Ross Harper <rossharper@users.noreply.github.com>
2020-02-15 13:07:59 +0000

    Read realtime offset from environment variable

--- callforheat.cpp
+++ callforheat.cpp
@@ -11,9 +11,13 @@ int main(int argc, char *argv[]) {
     int callforheat = atoi(argv[1]);
 
     int realtimeOffset = nDefaultNonRealtimeOffset;
+    if(const char* env_offset = getenv("CALLFORHEAT_OFFSET")) {
+        realtimeOffset = atoi(env_offset);
+    }
     if(argc > 2) {
         realtimeOffset = atoi(argv[2]);
     }
+    printf("Realtime offset: %d\n", realtimeOffset);
 
     TransmitPin* transmitPin = TransmitPinFactory::create(PIN, realtimeOffset);
     if(!transmitPin) return 1;
